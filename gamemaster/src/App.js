8fb038ea1594bdb715ccb62f610debbea33ac8d2
RickyBoy <rmsmp2003@gmail.com>
2024-10-24 09:42:16 +0100

    Create Character HTML partially done

--- gamemaster/src/App.js
+++ gamemaster/src/App.js
@@ -6,6 +6,8 @@ import SignInPage from "./pages/SignInPage";
 import LoginPage from "./pages/LoginPage";
 import HostOrPlayerPage from "./pages/HostOrPlayer";
 import Header from "./components/Header";
+import CreateCharacter from "./components/CreateCharacter";
+import CreateCharacterPage from "./pages/CreateCharacterPage";
 
 function App() {
   return (
@@ -14,7 +16,8 @@ function App() {
         <Routes>
           <Route path="/signin" element={<SignInPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<HostOrPlayerPage />} />
+          <Route path="/hostorplayer" element={<HostOrPlayerPage />} />
+          <Route path="/" element={<CreateCharacterPage />} />
         </Routes>
       </Router>
     </div>
---
+++ gamemaster/src/pages/CreateCharacterPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateCharacter from "../components/CreateCharacter";
+
+const CreateCharacterPage = () => {
+  return (
+    <div>
+      <CreateCharacter />
+    </div>
+  );
+};
+
+export default CreateCharacterPage;
