3ceba6bce8aed835a06c4d52477d2cee0f30275e
RickyBoy <rmsmp2003@gmail.com>
2024-10-27 17:59:30 +0000

    fixed

--- gamemaster/src/App.js
+++ gamemaster/src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
 import LoginPage from "./pages/LoginPage";
-import HostOrPlayerPage from "./pages/HostOrPlayer";
+import HostOrPlayerPage from "./pages/HostOrPlayerPage";
 import Header from "./components/Header";
-import CreateCharacter from "./components/CreateCharacter";
+import GamePage from "./pages/GamePage";
 import CreateCharacterPage from "./pages/CreateCharacterPage";
 import LobbyPage from "./pages/LobbyPage";
 
@@ -15,11 +15,12 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<SignInPage />} />
+          <Route path="/signin" element={<SignInPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/hostorplayer" element={<HostOrPlayerPage />} />
           <Route path="/lobby" element={<LobbyPage />} />
           <Route path="/createcharacter" element={<CreateCharacterPage />} />
+          <Route path="/game" element={<GamePage />} />
         </Routes>
       </Router>
     </div>
